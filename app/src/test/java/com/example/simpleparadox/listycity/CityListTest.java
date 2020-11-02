0e9cf9b56a2009ed4773d13e1d2e164507dc3ae5
zoeypu98 <72234393+zoeypu98@users.noreply.github.com>
2020-11-02 14:44:44 -0700

    Update

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -29,6 +29,19 @@ class CityListTest {
         assertTrue(cityList.hasCity(new City("Regina", "Saskatchewan")));
     }
     
+    
+     @Test
+    void testGetCities(){
+        CityList.cityList = mockCityList();
+        assertEquals(1, mockCity().compareTo(cityList.getCities().get(0)));
+        
+        City city = new City("Charlottetown", "Prince Edward Island");
+        cityList.add(city);
+        
+        assertEquals(0, city().compareTo(cityList.getCities().get(0)));
+        assertEquals(0, mockCity().compareTo(cityList.getCities().get(1)));        
+        
+    }
    
 }
     
