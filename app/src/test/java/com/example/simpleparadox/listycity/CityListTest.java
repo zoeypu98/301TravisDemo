6e8ec523556156d65521d62b5abb1d800c512ccf
Zoey Pu <zoeypu98@gmail.com>
2020-11-02 15:12:51 -0700

    lab exercise

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -31,8 +31,35 @@ class CityListTest {
     
     @Test
     void testGetCities(){
-        CityList.cityList = mockCityList();
+        CityList cityList = mockCityList();
         assertEquals(0, mockCity().compareTo(cityList.getCities().get(0)));
+
+        City city = new City("Charlottetown", "Prince Edward Island");
+        cityList.add(city);
+
+        assertEquals(0, city.compareTo(cityList.getCities().get(0)));
+        assertEquals(0, mockCity().compareTo(cityList.getCities().get(1)));
+
     }
+
+    @Test
+    void testDeleteCity(){
+        CityList cityList = mockCityList();
+        assertEquals(0, mockCity().compareTo(cityList.getCities().get(0)));
+
+        City city = new City("Vancouver", "British Columbia");
+        cityList.add(city);
+
+        assertEquals(2, cityList.countCities());
+
+        cityList.delete(mockCity());
+
+        assertEquals(1, cityList.countCities());
+        assertEquals(0, city.compareTo(cityList.getCities().get(0)));
+
+    }
+
+
+
 }
     
